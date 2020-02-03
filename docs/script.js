76cb7eea7739d5635721761145230fab5018d9ec
Sjors <sjorspieter@live.nl>
2020-02-03 16:01:09 +0100

    Added filter after selecting data once

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,16 @@
 import fetchData from './modules/data.js'
 
 fetchData()
-    .then(data => console.log(data))
+    .then(data => pickQuestion(data))
     .catch(error => console.log(error))
 
+function pickQuestion(data) {
+    let randomQuestionIndex = Math.floor(Math.random() * Math.floor(data.length))
+    let randomQuestion = data[Math.floor(Math.random() * Math.floor(data.length))]
+
+    data = data.filter((d, i) => i != randomQuestionIndex)
+
+    console.log(randomQuestionIndex)
+    console.log(randomQuestion)
+    return randomQuestion
+}
